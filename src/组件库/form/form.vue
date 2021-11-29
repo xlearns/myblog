bdf6aa82528c6914bf27950b3dbb26b213416c1a
xlearns <62782792+xlearns@users.noreply.github.com>
2021-11-29 01:25:25 -0800

    fix

---
+++ src/组件库/form/form.vue
@@ -0,0 +1,18 @@
+<template>
+    <form>
+        <slot></slot>
+    </form>
+</template>
+<script>
+export default defineComponent({
+    name:"uiForm",
+    props:{
+      roles:{
+           type: Object
+      },
+      modelValue:{
+          type: Object
+      }
+    }
+})
+</script>
---
+++ src/组件库/form/formItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+    <label v-if="label">{{ label }}</label>
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default defineComponent({
+    name:"uiFormItem",
+    props:{
+      prop:{
+           type: String
+      },
+      label:{
+          type:String,
+          default: ''
+      }
+    }
+})
+</script>
